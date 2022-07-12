be7d9887b8f4a31c83f2d9085d527cec4dde6add
ardanacar <74206181+ardanacar@users.noreply.github.com>
2022-07-12 14:09:39 +0200

    AtoB() returns coordinates relative to origin

--- TargetPositionEstimation/AtoB.cpp
+++ TargetPositionEstimation/AtoB.cpp
@@ -4,8 +4,7 @@
 using namespace cv;
 
 //rotations are expected to be given relative to origin in radians
-//coordinatesInB = inv * coordinatesInA
-void calcAtoB(Mat* inv, float* rb){
+void rotAtoB(Mat* inv, float* rb){
     float rx = -rb[0];
     float ry = -rb[1];
     float rz = -rb[2];
@@ -21,4 +20,34 @@ void calcAtoB(Mat* inv, float* rb){
     
     //calculate the combined inverse rotationn matrix
     *inv = rotMatX * rotMatY * rotMatZ;
+}
+
+//rotations are expected to be given relative to origin in meter
+void transAtoB(float* coordinates, float* tb){
+    float coordinates[0] += tb[0];
+    float coordinates[1] += tb[1];
+    float coordinates[2] += tb[2];
+}
+
+float* AtoB(float* resultMat, float* rb, float* tb){
+    //coordinates relative to camera
+    float* resultsA = {resultMat[3], resultMat[7], resultMat[11]};
+    Mat resultAf(Size(1,3), CV_32F, resultsA);
+
+    //calculate rotation matrix
+    float invf[3][3];
+    Mat inv(Size(3, 3), CV_32F, invf);
+    rotAtoB(&inv, rb);
+    
+    //allocate coordinates to be returned
+    float* coordinates = (float*)malloc(3 * sizeof(float));
+    
+    //coordinates after rotation but before translation
+    Mat beforeTrans(Size(1,3), CV_32F, coordinates);
+    beforeTrans = inv * resultAf;
+    
+    //translate to origin
+    transAtoB(coordinates, tb);
+    
+    return coordinates;
 }
--- TargetPositionEstimation/AtoB.h
+++ TargetPositionEstimation/AtoB.h
@@ -8,4 +8,20 @@
  * @param inv result as 3x3 matrix pointer
  * @param rb rotations in x,y,z in radians as 1x3 float array
  */
-void calcAtoB(Mat* inv, float* rb);
+void rotAtoB(Mat* inv, float* rb);
+
+/**
+ * computes the rotation matrix from A (camera) to B (origin) as 3x3 matrix
+ * @param coordinates relative to camera as 1x3 float array to be overwritten by the result
+ * @param tb translations relative to origin in x,y,z in meter as 1x3 float array
+ */
+void transAtoB(float* coordinates, float* tb);
+
+/**
+ * computes the coordinates relative to origin
+ * @param resultMat pose matrix relative to camera
+ * @param rb rotations in x,y,z in radians as 1x3 float array
+ * @param tb translations relative to origin in x,y,z in meter as 1x3 float array
+ * @return coordinates relative to origin
+ */
+float* AtoB(float* resultMat, float* rb, float* tb);
