8e9e1ad2427138e924c5be5dd8b4c9830ee2b53a
Max-p-otential <maximilian.pursche@tum.de>
2022-07-12 14:57:46 +0200

    Pushing added server functionality

--- CoordinateServer/CoordinateServer.cpp
+++ CoordinateServer/CoordinateServer.cpp
@@ -4,10 +4,21 @@
 
 #include "MarkerTracker.h"
 #include "AtoB.h"
+#include "AsyncCommunicationServer.h"
+#include "json.hpp"
 
+
+// JSON String that is sent to all Clients
+std::string POS_MESSAGE;
 
 int main()
 {
+	// setup server acceptor loop in own thread
+	boost::asio::io_context io_context;
+	tcp_server server(io_context);
+	std::thread my_thread([&]() { io_context.run(); });
+
+	// setup openCV marker tracker
 	cv::Mat img_bgr;
 	cv::VideoCapture cap;
 	cap.open(0);
@@ -15,11 +26,23 @@ int main()
 	float rotB[] = { 0.f, 0.f, 0.f };
 	float transB[] = { 0.f, 0.f, 0.f };
 
+
+
 	while (cap.read(img_bgr))
 	{
 		float resultMatrix[16];
 		markerTracker->findMarker(img_bgr, resultMatrix);
 		float* coordinates = AtoB(resultMatrix, rotB, transB);
+
+		// JSON object to parse
+		auto networkCoordinates = nlohmann::json::array();
+
+		// sending JSON String to all Clients
+		std::string jsonString = networkCoordinates.dump();
+		POS_MESSAGE = jsonString;
+		server.send_to_clients(POS_MESSAGE);
+
+
 
 		free(coordinates);
 		if (cv::waitKey(10) == 27)
